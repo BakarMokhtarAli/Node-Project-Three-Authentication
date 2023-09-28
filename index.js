24cc14278b7723e8203f22a1c2b566323d2cdce6
bakar mokhtar <walaalpakar08@gmail.com>
2023-09-28 15:49:16 +0300

    waan ka shaqeyay

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Import server here and start the application
+
+import server from "./api/server.js";
+
+const PORT = 8000;
+
+server.listen(PORT, ()=>{
+    console.log(`server runing on port ${PORT}`)
+})
